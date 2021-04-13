a79bb2ee4130c66e324145b3760cbb881836c93f
Hasar Ali <hasar.ali@protonmail.com>
2021-04-13 13:42:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,19 +11,13 @@ function move(node, refpt, amount, unit) {
 }
 
 function moveDodgerLeft() {
-  const node = dodger;
-  const refpt = 'left';
-  const amount = '1';
-  const unit = 'pt';
-  let val = node.style[refpt].replace(unit, '');
-  val = parseInt(val, 10);
-  
-  const nodeWidth = node.offsetWidth;
-  const parentWidth = node.parentNode.offsetWidth
-  
-  
-  if (val - amount >= 0 && val - amount <= parentWidth - nodeWidth)
-    node.style[refpt] = `${val - amount}${unit}`;
+  unction moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
 }
 
 function moveDodgerRight() {
